9439fc9347541126dd86a387e91876a669c08e68
youseefmoemen <youseefmoemen293@gmail.com>
2021-11-11 16:11:26 +0200

    CD is done

--- Parser.java
+++ Parser.java
@@ -2,15 +2,19 @@ public class Parser{
     String commandName;
     String [] args;
     public Boolean parse(String input){
-        return true;
+        String[] hold = input.split(" ");
+        this.commandName = hold[0];
+        
+        this.args = new String[hold.length - 1];
+        for(int i = 0; i < this.args.length; i++){
+            args[i] = hold[i+1];
+        }
+        return true; //Why might cause False
     }
     public String getCommandName(){
-        return "Good";
+        return this.commandName;
     }
-
-    public String getArgs(){
-        return "Good";
+    public String[] getArgs(){ //Modified due to args is string array not only one string
+        return this.args;
     }
-
-
 }
--- Terminal.java
+++ Terminal.java
@@ -1,14 +1,47 @@
 //Youseeef: cd, mkdir, cp, cp-r
 //Amin: rm, cat, echo, ls
 //Solhf: pwd, ls-r, rmdir, touch
+import java.util.*;
+import java.io.File;
 public class Terminal{
+    File currentPath;
+
     public String pwd(){
         return "Hello";
     }
     
-    public void echo()
+    public Boolean cd(String path){
+        File hold = new File(this.currentPath.getAbsolutePath()+ "/" + path);
+        if (hold.isDirectory()){
+            this.currentPath = hold;
+            return true;
+        }
+        hold = new File(path);
+        if(hold.isDirectory()){
+            this.currentPath = hold;
+            return true;
+        }
+        return false;
+    }
+    public void chooseCommandAction(Parser parser){ // added the parser to be abel to access args
+        if (parser.getCommandName().equals("cd")){
+            Boolean state = cd(parser.args[0]);
+            if (state == false){
+                System.out.println("Invalid Directory");
+            }
+        }
+    }
     public static void main(String[] args){
-        Parser parse = new Parser();
-        System.out.println(parse.getArgs());
+        Terminal terminal = new Terminal();
+        terminal.currentPath = new File(System.getProperty("user.dir"));  
+        Scanner scanner = new Scanner(System.in);
+        String input = new String();
+        Parser parser = new Parser();
+        while (true){
+            System.out.println(terminal.currentPath);
+            input = scanner.nextLine();
+            parser.parse(input);
+            terminal.chooseCommandAction(parser);
+        }
     }
 }
