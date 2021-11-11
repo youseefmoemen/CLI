e6b3c8df6fe58ccd2f3a32b80a2ac0fe7ef89255
youseefmoemen <youseefmoemen293@gmail.com>
2021-11-11 06:56:05 +0200

    files uploading

---
+++ Parser.java
@@ -0,0 +1,18 @@
+public class Parser{
+    String commandName;
+    String [] args;
+    public Boolean parse(String input){
+        return true;
+    }
+    public String getCommandName(){
+        return "Good";
+    }
+
+    public String getArgs(){
+        return "Good";
+    }
+
+    public static void main(String[] args){
+        System.out.println("Hello, Java");
+    }
+}
