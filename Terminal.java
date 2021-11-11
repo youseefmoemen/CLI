d49d2a8f91e2ca139637c0460443a62d21d25a4e
youseefmoemen <youseefmoemen293@gmail.com>
2021-11-11 07:58:16 +0200

    upload

--- Terminal.java
+++ Terminal.java
@@ -1,10 +1,12 @@
-import Parser.java;
-
-
+//Youseeef: cd, mkdir, cp, cp-r
+//Amin: rm, cat, echo, ls
+//Solhf: pwd, ls-r, rmdir, touch
 public class Terminal{
     public String pwd(){
         return "Hello";
     }
+    
+    public void echo()
     public static void main(String[] args){
         Parser parse = new Parser();
         System.out.println(parse.getArgs());
