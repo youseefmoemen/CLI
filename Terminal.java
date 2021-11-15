959c171b8fdb8e7c3bff899aba9f9fb7d20b8817
Alameen7007 <nemoshsrk@gmail.com>
2021-11-15 06:53:57 +0200

    Some last touches.
    Solhf's part is the only left

--- Terminal.java
+++ Terminal.java
@@ -13,13 +13,125 @@ import java.nio.file.StandardCopyOption;
 public class Terminal{
     File currentPath;
     Boolean flag = true;
-    public void cp_r(String[] folders){
-        if (folders.length != 3){ // -r f1 f2
+
+    public void echo(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.print(args[i]);
+            System.out.print(' ');
+        }
+        System.out.println();
+    }
+
+    public void pwd(String[] args) {
+        if (args.length == 0) {
+            System.out.println(currentPath);
+        } else
+            System.out.println("Too Many Arguements");
+    }
+
+    // Can't ls when i go back in cd..
+    public Boolean cd(String args) {
+        if (args.equals("")) {
+            this.currentPath = new File("C:\\");
+            return true;
+        }
+        if (args.equals("..")) {
+            String pervious = this.currentPath.getAbsolutePath().substring(0,
+                    this.currentPath.getAbsolutePath().lastIndexOf('\\'));
+            if (pervious.equals("")) {
+                System.out.println("NO pervious path!");
+                return true;
+            }
+            this.currentPath = new File(pervious);
+            return true;
+        }
+        File file = new File(this.currentPath.getAbsolutePath() + "\\" + args);
+        if (file.isDirectory()) {
+            this.currentPath = file;
+            return true;
+        }
+        file = new File(args);
+        if (file.isDirectory()) {
+            this.currentPath = file;
+            return true;
+        }
+        return false;
+    }
+
+    public void ls(String[] args) {
+        if (args.length == 0) {
+            File[] files = new File(currentPath.toString()).listFiles();
+
+            for (int i = 0; i < files.length; i++) {
+                System.out.println(files[i].getName());
+            }
+        } else
+            System.out.println("Too Many Arguements");
+    }
+
+    public void mkdir(String[] args) {
+        for (String file : args) {
+            int val = file.lastIndexOf("/");
+            if (val == -1) {
+                File newFolder = new File(this.currentPath.getAbsolutePath() + "/" + file);
+                if (newFolder.exists()) {
+                    System.out.println(file + " Already exist");
+                } else {
+                    newFolder.mkdirs();
+                }
+            } else {
+                String path = file.substring(0, file.lastIndexOf("/"));
+                File check = new File(path);
+                if (!check.isDirectory()) {
+                    System.out.println("Invalid path: " + path);
+                    continue;
+                }
+                File newFolder = new File(file);
+                if (newFolder.exists()) {
+                    System.out.println(file + " Already exist");
+                } else {
+                    newFolder.mkdirs();
+                }
+            }
+        }
+    }
+    
+    public void cp(String [] args){
+        //TODO check the path for the dest is correct
+        if(args.length != 2){
+            System.out.println("Unvalid arguments only two files allowed");
+            return;
+        }
+        File file1 = new File(args[0]);
+        File file2 = new File(args[1]);
+        Scanner reader;
+        try {
+            reader = new Scanner(file1);
+        } catch (FileNotFoundException e) {
+            System.out.println("Source file not exist");
+            return;
+        }
+        try{
+            FileWriter myWriter = new FileWriter(file2);
+            while(reader.hasNextLine()){
+                myWriter.write(reader.nextLine());
+                myWriter.write("\n");
+            }
+            reader.close();
+            myWriter.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    //TODO cd error
+    
+    public void cp_r(String[] args){
+        if (args.length != 3){ // -r f1 f2
             System.out.println("Unvalid arguments only two paths allowed");
             return;
         }
-        File file1 = new File(folders[1]);
-        File file2 = new File(folders[2]);
+        File file1 = new File(args[1]);
+        File file2 = new File(args[2]);
         if(!file1.isDirectory()){
             System.out.println("Unvalid Source folder");
             return;
@@ -46,159 +158,38 @@ public class Terminal{
         }
 
     }
-    
-    public void cp(String [] files){
-        //TODO check the path for the dest is correct
-        if(files.length != 2){
-            System.out.println("Unvalid arguments only two files allowed");
-            return;
-        }
-        File file1 = new File(files[0]);
-        File file2 = new File(files[1]);
-        Scanner reader;
-        try {
-            reader = new Scanner(file1);
-        } catch (FileNotFoundException e) {
-            System.out.println("Source file not exist");
-            return;
-        }
-        try{
-            FileWriter myWriter = new FileWriter(file2);
-            while(reader.hasNextLine()){
-                myWriter.write(reader.nextLine());
-                myWriter.write("\n");
-            }
-            reader.close();
-            myWriter.close();
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-    }
-    //TODO cd error
-    public void mkdir(String[] dirs){
-        for(String file: dirs){
-            int val = file.lastIndexOf("/");
-            if(val == -1){
-                File newFolder = new File(this.currentPath.getAbsolutePath() + "/" + file);
-                if(newFolder.exists()){
-                    System.out.println(file + " Already exist");
-                }else{
-                    newFolder.mkdirs();
-                }
-            }else{
-                String path = file.substring(0, file.lastIndexOf("/"));
-                File check = new File(path);
-                if(!check.isDirectory()){
-                    System.out.println("Invalid path: " + path);
-                    continue;
-                }
-                File newFolder = new File(file);
-                if(newFolder.exists()){
-                    System.out.println(file + " Already exist");
-                }else{
-                    newFolder.mkdirs();
-                }
-            }
-        }
-    }
-    
-    public Boolean cd(String path){
-        if (path.equals("")){
-            this.currentPath = new File("C:\\");
-            return true;
-        }
-        if(path.equals("..")){
-            String pervious = this.currentPath.getAbsolutePath().substring(0,
-            this.currentPath.getAbsolutePath().lastIndexOf('/'));
-            if(pervious.equals("")){
-                System.out.println("NO pervious path!");
-                return true;
-            }
-            this.currentPath = new File(pervious);
-            return true;
-        }
-        File hold = new File(this.currentPath.getAbsolutePath()+ "/" + path);
-        if (hold.isDirectory()){
-            this.currentPath = hold;
-            return true;
-        }
-        hold = new File(path);
-        if(hold.isDirectory()){
-            this.currentPath = hold;
-            return true;
-        }
-        return false;
-    }
-    
-    public void ls(String args[]) {
-        if(args.length == 0){
-            File[] files = new File(currentPath.toString()).listFiles();
 
-            for (int i = 0; i < files.length; i++) {
-                System.out.println(files[i].getName());
-            }
-        } else System.out.println("Too Many Arguements");
-    }
-
-    public void pwd(String args[]) {
-        if (args.length == 0) {
-            System.out.println(currentPath);
-        } else
-            System.out.println("Too Many Arguements");
-    }
-
-    public void echo(String[] sentence){
-        for(int i = 0; i  < sentence.length; i++){
-            System.out.print(sentence[i]);
-            System.out.print(' ');
-        }
-        System.out.println();
-    }
-
-    public void rm(String[] files){
-       if(files.length == 1) {
-           File f = new File(files[0]);
-           if (!(f.isAbsolute()))
-               f = new File(currentPath + "\\" + files[0]);
-           if (f.delete()) {
-               if (f.isDirectory())
+    public void rm(String[] args){
+       if(args.length == 1) {
+           File file = new File(args[0]);
+           if (!(file.isAbsolute()))
+               file = new File(currentPath + "\\" + args[0]);
+           if (file.delete()) {
+               if (file.isDirectory())
                    System.out.println("Cannot Delete a Directory");
                else
-                   f.delete();
+                   file.delete();
            } else
                System.out.println("Could not delete file");
         } else System.out.println("Wrong Number of Arguements");
     }
 
-    public void cat(String[] files) throws IOException {
-        if (files.length == 1) {
-            File file = new File(files[0]);
-            if (!(file.isAbsolute()))
-                file = new File(currentPath + "\\" + files[0]);
-            FileInputStream fis = new FileInputStream(file);
-
-            int oneByte;
-            while ((oneByte = fis.read()) != -1) {
-                System.out.write(oneByte);
-            }
-            fis.close();
-            System.out.flush();
-            System.out.println();
-        }
-        else if (files.length == 2){
-            for(int i = 0; i < 2; i++){
-                File file = new File(files[i]);
-                if (!(file.isAbsolute()))
-                    file = new File(currentPath + "\\" + files[i]);
-                FileInputStream fis = new FileInputStream(file);
-
-                int oneByte;
-                while ((oneByte = fis.read()) != -1) {
-                    System.out.write(oneByte);
-                }
-                fis.close();
-                System.out.flush();
-                System.out.println();
+    public void cat(String[] args) throws IOException {
+        if (args.length == 1 || args.length == 2){
+            for(int i = 0, n = args.length; i < n; i++){
+                File file = new File(args[i]);
+                if (!(file.isAbsolute())) file = new File(currentPath + "\\" + args[i]);
+                
+                if(file.exists()){
+                    FileInputStream fis = new FileInputStream(file);
+                    int oneByte;
+                    while ((oneByte = fis.read()) != -1) {
+                        System.out.write(oneByte);
+                    }
+                    fis.close();
+                    System.out.flush();
+                    System.out.println();
+                } else System.out.println("File Does Not Exist");
             }
         }
         else System.out.println("Wrong number of arguments");
